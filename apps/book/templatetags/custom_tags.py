d7b702209f294c47433f4c7c6f5a63adedea9792
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2024-07-17 08:23:51 +0100

    django templates

--- apps/book/templatetags/custom_tags.py
+++ apps/book/templatetags/custom_tags.py
@@ -8,3 +8,7 @@ register = template.Library()
 @register.filter
 def semi_colon_separator(value):
     return value.replace(', ', '; ')
+
+@register.filter
+def join_fields(values):
+    return ', '.join(str(value) for value in values)
--- apps/book/views.py
+++ apps/book/views.py
@@ -8,8 +8,6 @@ def book_detail(request, isbn):
 
     context = {
         "book": book,
-        "tags": ', '.join(str(tag) for tag in book.tags.all()),
-        "authors": ', '.join(str(author) for author in book.authors.all())
 
     }
     return render(request, 'book_detail.html', context)
